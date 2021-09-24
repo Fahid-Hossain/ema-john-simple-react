2032f8d5e9551a15f374faa41f35036ca0e22244
Fahid Hossain <fahidhossain2021@gmail.com>
2021-09-24 14:50:43 +0600

    rating set with font-awesome icon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import "./Product.css"
+import Rating from 'react-rating';
 
 const Product = (props) => {
     const product = props.product;
     //destructuring 
-    const {name,img,seller,price,stock}=product;
+    const {name,img,seller,price,stock,star}=product;
     return (
         <div className="products">
             <div className="product-img">
@@ -18,6 +19,8 @@ const Product = (props) => {
             <p>by: {seller}</p>
             <h2>${price}</h2>
             <p>only {stock} left in stock - order soon</p>
+            <Rating initialRating={star} readonly   emptySymbol="far fa-star icon-color" fullSymbol="fas fa-star icon-color"></Rating>
+            <br />
             <button onClick={()=>props.handlerAddToCart(product)} className="purchase-btn"><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
 
             </div>
