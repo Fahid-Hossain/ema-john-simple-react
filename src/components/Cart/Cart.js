71b647eb61fbf1c33db38e8e3a9537598e28e7d7
Fahid Hossain <fahidhossain2021@gmail.com>
2021-09-24 14:24:59 +0600

    order summary save to localStorage and search funtionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,25 +4,37 @@ import "./Cart.css"
 const Cart = (props) => {
     // console.log(props.cart)
     const {cart} = props;
+    console.log(cart)
 
     // for loop for total price
     // let total = 0;
     // for (const product of cart){
     //    total = total + product.price;
     // }
+    // console.log(cart)
+
+    // // reduce for total price 
+    // const reducer = (previous,current)=>previous + current.price;
+    // const total = cart.reduce(reducer,0);
+
+    // for total quantity of product
+    let total= 0;
+    let totalQuantity = 0;
+    for (const product of cart){
+        if(!product.quantity){
+            product.quantity=1;
+        }
+            total = total + product.price * product.quantity;
+            totalQuantity = totalQuantity + product.quantity;
+    }
     
-    // reduce for total price 
-    const reducer = (previous,current)=>previous + current.price;
-    const total = cart.reduce(reducer,0);
-
-
     const shipping = total/30;
     const tax = total/35;
     const grandTotal =(total+tax+shipping);
     return (
         <div>
             <h1>Order Summary</h1>
-            <p>Items Oredered:{props.cart.length} </p>
+            <p>Items Oredered:{totalQuantity} </p>
             <h3>Price:${total.toFixed(2)}</h3>
             <h3>Shipping: ${shipping.toFixed(2)}</h3>
             <h3>Tax: ${tax.toFixed(2)}</h3>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import "./Shop.css"
@@ -8,25 +9,71 @@ const Shop = () => {
     const [products,setProducts]=useState([])
     //useState for carts
     const [cart,setCart]=useState([])
-    //useEffect 
+    
+    //useState for filterProduct 
+    const [filterProduts,setFilterProducts]=useState([]);
+    //useEffect for products data fetch
     useEffect(()=>{
         fetch("./products.JSON")
         .then(res=>res.json())
-        .then(data=>setProducts(data))
+        .then(data=>{
+            setFilterProducts(data)
+            setProducts(data)
+        })
     },[])
+
+    //useEffect for localStorage data fetch
+    useEffect(()=>{
+    if(products.length){
+       const addedProduct = getStoredCart();
+    //    console.log(addedProduct);
+    const storedCart = [];
+    for (const key in addedProduct){
+        // console.log(key)
+        // console.log(products)
+            let foundProduct = products.find(product=>product.key===key);
+            // console.log(key,foundProduct)
+            if(foundProduct){
+                const quantity = addedProduct[key];
+                foundProduct.quantity=quantity;
+                // console.log(foundProduct)
+                storedCart.push(foundProduct);
+            }
+        }
+
+        setCart(storedCart)
+    }
+
+    },[products])
 
     // onclick handler add to cart 
     const handlerAddToCart = (product)=>{
         // console.log("product click",product);
         const newCart = [...cart,product];
         setCart(newCart)
+        // add to localStorage as database
+        addToDb(product.key)
     }
+
+    // search handler 
+    const searchHandler = (event)=>{
+        const searchText = (event.target.value);
+        const matchedProduct =  products.filter(product=>product.name.toLowerCase().includes(searchText.toLowerCase()))
+        // console.log(matchedProduct);
+        setFilterProducts(matchedProduct);
+    }
+
+
     return (
+        <div>
+            <div className="search-container">
+                <input onChange={searchHandler} type="text" />
+            </div>
         <div className="product-container">
             <div className="product">
                 <h1>Products:{products.length}</h1>
                 {
-                    products.map(product=><Product
+                    filterProduts.map(product=><Product
                          product={product} 
                          key={product.key}
                          handlerAddToCart={handlerAddToCart}
@@ -37,6 +84,8 @@ const Shop = () => {
             <div className="cart">
                 <Cart cart={cart}></Cart>
             </div>
+        </div>
+
         </div>
     );
 };
